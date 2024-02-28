cb7ade12fd51606f8c4ecdc9a65f5a7df6c7f4b3
doesitbyte <kaustubhsharma999@gmail.com>
2024-02-28 16:06:24 +0530

    Added inputs + tags

--- code/src/functions/product_review/index.ts
+++ code/src/functions/product_review/index.ts
@@ -6,6 +6,9 @@ export async function handleEvent(event: any) {
   const APIBase = event.execution_metadata.devrev_endpoint;
   const apiUtil: ApiUtils = new ApiUtils(APIBase, devrevPAT);
 
+  const tags = event.input_data.resources.tags;
+  const inputs = event.input_data.global_values;
+
   const snapInId = event.context.snap_in_id;
   let parameters: string = event.payload.parameters.trim();
   let product: string | undefined;
@@ -24,6 +27,31 @@ export async function handleEvent(event: any) {
     return;
   }
   commentID = postResp.data.timeline_entry.id;
+
+  const createTicketResp = await apiUtil.createTicket({
+    title: product,
+    tags: [{ id: tags['test_tag1'].id }],
+    body: 'test_body',
+    type: publicSDK.WorkType.Ticket,
+    owned_by: [inputs['default_owner_id']],
+    applies_to_part: inputs['default_part_id'],
+  });
+  if (!createTicketResp.success) {
+    console.error(`Error while creating ticket: ${createTicketResp.message}`);
+    return;
+  }
+  // Post a message with ticket ID.
+  const ticketID = createTicketResp.data.work.id;
+  const ticketCreatedMessage = `Created ticket: <${ticketID}> for product ${product}`;
+  const postTicketResp: HTTPResponse = await apiUtil.postTextMessageWithVisibilityTimeout(
+    snapInId,
+    ticketCreatedMessage,
+    1
+  );
+  if (!postTicketResp.success) {
+    console.error(`Error while creating timeline entry: ${postTicketResp.message}`);
+    return;
+  }
 }
 
 export const run = async (events: any[]) => {
